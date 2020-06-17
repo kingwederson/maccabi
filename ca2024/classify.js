158d9631fae2219b82ddb419c31388cd1582b51d
kingwederson <56653848+kingwederson@users.noreply.github.com>
2020-06-16 22:35:30 -0300

    Add files via upload

--- ca2024/classify.js
+++ ca2024/classify.js
@@ -1,9 +1,9 @@
 function classifica(){
-    var vitorias = [/*ARG*/1,/*BOL*/0,/*COL*/0,/*EQU*/0,/*EUA*/1,/*PAR*/0,/*BRA*/1,/*CHI*/0,/*MEX*/0,/*PER*/0,/*URU*/0,/*VEN*/0]
+    var vitorias = [/*ARG*/1,/*BOL*/0,/*COL*/0,/*EQU*/0,/*EUA*/1,/*PAR*/0,/*BRA*/1,/*CHI*/0,/*MEX*/0,/*PER*/0,/*URU*/1,/*VEN*/0]
     var empates = [/*ARG*/0,/*BOL*/0,/*COL*/1,/*EQU*/0,/*EUA*/0,/*PAR*/1,/*BRA*/0,/*CHI*/1,/*MEX*/0,/*PER*/1,/*URU*/0,/*VEN*/0]
-    var derrotas = [/*ARG*/0,/*BOL*/1,/*COL*/0,/*EQU*/1,/*EUA*/0,/*PAR*/0,/*BRA*/0,/*CHI*/0,/*MEX*/1,/*PER*/0,/*URU*/0,/*VEN*/0]
-    var golpro = [/*ARG*/3,/*BOL*/0,/*COL*/2,/*EQU*/0,/*EUA*/4,/*PAR*/2,/*BRA*/2,/*CHI*/0,/*MEX*/1,/*PER*/0,/*URU*/0,/*VEN*/0]
-    var golcontra = [/*ARG*/1,/*BOL*/3,/*COL*/2,/*EQU*/4,/*EUA*/0,/*PAR*/2,/*BRA*/1,/*CHI*/0,/*MEX*/2,/*PER*/0,/*URU*/0,/*VEN*/0]
+    var derrotas = [/*ARG*/0,/*BOL*/1,/*COL*/0,/*EQU*/1,/*EUA*/0,/*PAR*/0,/*BRA*/0,/*CHI*/0,/*MEX*/1,/*PER*/0,/*URU*/0,/*VEN*/1]
+    var golpro = [/*ARG*/3,/*BOL*/0,/*COL*/2,/*EQU*/0,/*EUA*/4,/*PAR*/2,/*BRA*/2,/*CHI*/0,/*MEX*/1,/*PER*/0,/*URU*/2,/*VEN*/1]
+    var golcontra = [/*ARG*/1,/*BOL*/3,/*COL*/2,/*EQU*/4,/*EUA*/0,/*PAR*/2,/*BRA*/1,/*CHI*/0,/*MEX*/2,/*PER*/0,/*URU*/1,/*VEN*/2]
 
     for (var i = 0;i<12;i++){
         var identidade = 'eq'+Number(i+10)+'P'
